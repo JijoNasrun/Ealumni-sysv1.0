df93726bc0351a778f77ad90cb6898a328d620e6
JijoNasrun <jijohakim@gmail.com>
2016-04-29 15:30:53 +0800

    latest update

--- T&C.php
+++ T&C.php
@@ -4,76 +4,98 @@
 <title>E-Alumni FSKM</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0">
-<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
 <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all">
 <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css'>
-<script src="js/jquery-1.8.0.min.js"></script>
+
 <!--[if lt IE 9]><script src="js/modernizr.custom.js"></script><![endif]-->
 <script src="js/jquery.flexslider-min.js"></script>
 <script src="js/functions.js"></script>
 <script src="alertify.js/lib/alertify.min.js"></script>
 <link rel="stylesheet" type="text/css" href="alertify.js/themes/alertify.css" />
 <link rel="stylesheet" type="text/css" href="alertify.js/themes/alertify.default.css" />
-
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <link href="check/css/style.css" rel='stylesheet' type='text/css' />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <!--webfonts-->
     <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text.css'/>
     <!--//webfonts-->
+    <style type="text/css">
+    .jumbotron {
+   background: rgb(200, 54, 54); /* This is for ie8 and below */
+   background: rgba(54, 54, 54, 0.5); 
+}
+  </style>
 
 </head>
 <body>
-<div id="wrapper">
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">E-Alumni FSKM</a>
+    </div>
+    <ul class="nav navbar-nav">
+    
+    </ul>
+  </div>
+</nav>
+
 
   <!-- main -->
    <center>
-    <div class="main">
+   
     
 
         
         
          
-         <fieldset>
-         <legend><h3>Terms And Condition</h3></legend>
+         <div class="container-fluid">
+         <div class="jumbotron">
+         
          <font color="black">
-            <h1>Privacy Policy</h1><br>
+         <div class="row" align="center">
+         <div class="col-md-12" align="center">
+         <h3>Terms And Condition</h3>
+            <h3>Privacy Policy</h3><br>
             All personal information (i.e. your address, phone number, e-mail) collected in the FSKM e-Alumni system will be used for the purposes of registration.
             We may also use the information you provide to fulfill further requests for information and services, or to contact you with information that may be of interest to you
             The information collected is maintained in confidence and in accordance with the <a href ="http://www.uitm.edu.my/index.php/en/component/content/article?layout=edit&id=356" target="_blank">Privacy Policies of the UiTM</a>.<br>
-            <br><h1>Contact Us</h1><br>
+            <br><h3>Contact Us</h3><br>
             For assistance with your registration including cancelations, or updating your information, our customer support team can be reached by e-mail, alumni@fskm.uitm.edu.my or phone, (+603) 5543 5497, Monday to Friday, 8:30 a.m. to 4:30 p.m.
             </font>
-         <fieldset>
-         
+            </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">&nbsp</div>
+            </div>
+            <div class="row" align="center">
+            <div class="col-md-4"></div>
+            <div class="col-md-1"></div>
+            <div class="col-md-2" align="center">
+              <a href="check.php" class="btn btn-success btn-block">Agree</a>
+            </div>
+            <div class="col-md-4"></div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">&nbsp</div>
+            </div>
+            <div class="row" align="center">
+            <div class="col-md-4"></div>
+            <div class="col-md-1"></div>
+            <div class="col-md-2" align="center">
+            <a href="index.html" class="btn btn-warning btn-block">Decline</a>
+            </div>
+            <div class="col-md-4"></div>
+        </div> 
+         </div>
+         </div></font>
+         </div>
 
-      <div class="header-cnt">
-        <a href="check.php" class="blue-btn">Agree</a>
-      
-        <a href="index.html" class="blue-btn">Decline</a>
-      </div>  
-      
-      
-      
-      
-   
-        
-        
-       
-      
           
-        
-   
-  </div>   
+  
   </center>      
-
-  <!-- end of main -->
-
-  <!-- footer-push -->
-  <div id="footer-push"></div>
-  <!-- end of footer-push -->
-</div>
-<!-- end of wrapper -->
-
 </body>
 </html>
