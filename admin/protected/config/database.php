988c1eb8c9a8ea98a25d709aa9f9033c468edc69
imaprog <shafiqaziz06@gmail.com>
2016-05-05 23:25:20 +0800

    fixed common mistakes

--- admin/protected/config/database.php
+++ admin/protected/config/database.php
@@ -5,9 +5,9 @@ return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	
-	'connectionString' => 'mysql:host=localhost;dbname=ea',
+	'connectionString' => 'mysql:host=localhost;dbname=ealumni',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => '12345',
 	'charset' => 'utf8',
 );
